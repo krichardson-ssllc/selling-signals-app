c0ae7b559b37c6d1c259b6fb655acbd0bc43469a
krichardson-ssllc <krichardson@ssllc.com>
2026-04-13 23:52:39 -0400

    Update filters.py

--- utils/filters.py
+++ utils/filters.py
@@ -1,1 +1,18 @@
+from datetime import datetime, timedelta
 
+def filter_by_recency(df, recency):
+    if recency == "All":
+        return df
+
+    days_map = {
+        30: 30,
+        60: 60,
+        90: 90,
+        180: 180
+    }
+
+    if recency not in days_map:
+        return df
+
+    cutoff = datetime.today() - timedelta(days=days_map[recency])
+    return df[df["Trigger Date"] >= cutoff]
